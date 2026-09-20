1b9ee97943851f2bdb47b39b41768fe90ac81234
MohamadKalakesh <106426856+MohamadKalakesh@users.noreply.github.com>
2026-09-20 13:42:51 +0300

    adding 2 cups

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const orderButton = document.getElementById('order-button');
 const orderSummary = document.getElementById('order-summary');
 
 orderButton.addEventListener('click', () => {
-  cups += 1;
+  cups += 2;
   const total = cups * price;
   orderSummary.textContent = `${cups} ${cups === 1 ? 'coffee' : 'coffees'} — Total: $${total}`;
 });
